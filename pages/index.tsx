e75fabf77c956012000bdc6c1be4b6f487dcfb35
gyurisc <gyurisc@gmail.com>
2023-03-13 12:33:45 +0100

    addin mainpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
+import Image from "next/image";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Github from "@/components/GitHub";
+import { NextPage } from "next";
+import { useState, useRef } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const Home: NextPage = () => {
 
-export default function Home() {
+
   return (
     <div className="mx-auto flex min-h-screen max-w-5xl flex-col pt-8 sm:pt-12">
       <Head>
@@ -12,9 +16,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className="mx-auto mt-10 flex max-w-5xl flex-1 flex-col justify-center px-2 sm:mt-40">
-        Summarize This!
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
+        <a
+          className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
+          href="https://github.com/gyurisc/summarizer-tool"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github />
+          <p>Star on GitHub</p>
+        </a>
+        <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
+          Generate your meeting summaries and todos using chatGPT
+        </h1>
+        <p className="text-slate-500 mt-5">Countless summaries generated so far.</p>
+
       </main>
+      <Footer />
     </div>
   );
 }
+
+export default Home;
